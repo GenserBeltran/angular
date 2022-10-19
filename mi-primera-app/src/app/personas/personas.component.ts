25893ec2fc13cabcf4a3ea3a2c969f177abe03a8
Daniel Beltran <genserdanie-123@hotmail.com>
2022-10-19 18:12:52 +0200

    Conceptos fundamentos instalacion util angular

---
+++ mi-primera-app/src/app/personas/personas.component.ts
@@ -0,0 +1,12 @@
+import { style } from "@angular/animations";
+import { Component } from "@angular/core";
+
+ @Component({
+selector: 'app-personas',
+templateUrl: './personas.component.html',
+styleUrls: [`./personas.component.css`],
+styles: [`h1{color: blue;}`]
+ })
+ export class PersonasComponent{
+
+ }
